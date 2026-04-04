639942c3e5edfd88530a0775eec4934e0d122f34
logicguild733-jpg <logicguild733@gmail.com>
2026-04-05 00:46:18 +0500

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// src/App.tsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Leads from "./pages/Leads";
+import NotFound from "./pages/NotFound"; // optional 404 page
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/leads" element={<Leads />} />
+        {/* Optional catch-all route for wrong URLs */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
