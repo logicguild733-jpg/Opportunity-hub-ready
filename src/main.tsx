df6dfa306a4f22e684794c591f0ba8e00abe3fc4
logicguild733-jpg <logicguild733@gmail.com>
2026-04-06 01:03:43 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,18 @@
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+// Find the root div from index.html
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+
+// Render the app
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
