f6022e847d76499ff7fb2887cc768332656981a3
logicguild733-jpg <logicguild733@gmail.com>
2026-04-09 21:16:19 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
@@ -6,10 +7,16 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "src"), // allows "@/..." imports
     },
   },
   build: {
     outDir: "dist",
+    rollupOptions: {
+      input: path.resolve(__dirname, "src/main.tsx"), // <-- ensures main.tsx is found
+    },
+  },
+  server: {
+    port: 3000,
   },
 });
