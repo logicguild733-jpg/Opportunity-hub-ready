d442f7c5137be347a5ada05608bbb50ee77fd3fa
logicguild733-jpg <logicguild733@gmail.com>
2026-05-07 23:01:44 +0500

    Update Supabase key to a new value

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,6 @@
-// src/lib/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://qnkxrxxwfikhrlirfleg.supabase.co";
-const supabaseKey = "sb_publishable_exYuiUhOVuWEyPqROu4p5A_gCWtb89S";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFua3hyeHh3ZmlraHJsaXJmbGVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5MTc4NDcsImV4cCI6MjA4NzQ5Mzg0N30.FX-ZcM-w6ci01NpYP9aAwgTzIx-I7Ir00YUm_fcmd7I";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
