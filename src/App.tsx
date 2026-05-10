8d4c7aa2d7dc0c4bcae5f36c5f1dc4f062108719
logicguild733-jpg <logicguild733@gmail.com>
2026-05-10 14:48:13 +0500

    Add routing with Routes and Route components

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,11 @@
+import { Routes, Route } from "react-router-dom";
 
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<div>Home OK</div>} />
+      <Route path="/login" element={<div>Login OK</div>} />
+      <Route path="/dashboard" element={<div>Dashboard OK</div>} />
+    </Routes>
+  );
+}
