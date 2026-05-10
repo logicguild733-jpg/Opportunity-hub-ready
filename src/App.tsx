9b69119050ad074736b9bcb4fbde0e0edd3caded
logicguild733-jpg <logicguild733@gmail.com>
2026-05-10 14:47:21 +0500

    Remove routing setup from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,1 @@
-import { Routes, Route } from "react-router-dom";
-export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<div>Home OK</div>} />
-      <Route path="/login" element={<div>Login OK</div>} />
-      <Route path="/dashboard" element={<div>Dashboard OK</div>} />
-    </Routes>
-  );
-}
+
