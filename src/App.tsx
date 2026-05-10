77385c529cbb825087214a76358658fb98ec0b08
logicguild733-jpg <logicguild733@gmail.com>
2026-05-10 23:54:44 +0500

    Refactor App component to log and simplify output

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import { Routes, Route } from "react-router-dom";
-
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<div>Home OK</div>} />
-      <Route path="/login" element={<div>Login OK</div>} />
-      <Route path="/dashboard" element={<div>Dashboard OK</div>} />
-    </Routes>
-  );
+  console.log("APP MOUNTED");
+
+  return <div>APP IS WORKING</div>;
 }
