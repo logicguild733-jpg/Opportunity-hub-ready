743ca19609701bc5a3c17b33ca6d6775e2357694
logicguild733-jpg <logicguild733@gmail.com>
2026-05-11 00:13:04 +0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,13 @@
-export default function App() {
-  console.log("APP MOUNTED");
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
 
-  return <div>APP IS WORKING</div>;
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
 }
