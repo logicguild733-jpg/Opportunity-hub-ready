e1b261c13bcc1efb3b84b6696623e47aab0b6d42
logicguild733-jpg <logicguild733@gmail.com>
2026-05-10 14:45:13 +0500

    Implement routing with React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { Routes, Route } from "react-router-dom";
 export default function App() {
-  return <div>🚀 APP WORKING</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<div>Home OK</div>} />
+      <Route path="/login" element={<div>Login OK</div>} />
+      <Route path="/dashboard" element={<div>Dashboard OK</div>} />
+    </Routes>
+  );
 }
